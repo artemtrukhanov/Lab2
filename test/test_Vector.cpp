8a50dfa0d181b3e6e808ca6685f5bb9c05324fdb
Artem <artem_trukhanov@mail.ru>
2020-10-24 21:32:48 +0300

    version 4

--- test/test_Vector.cpp
+++ test/test_Vector.cpp
@@ -76,7 +76,7 @@ TEST(Vector, throws_when_set_element_with_too_large_index)
 
 TEST(Vector, can_assign_vector_to_itself)
 {
-    ASSERT_NO_THROW(Vector<Vector<int>> v());
+    ASSERT_NO_THROW(Vector<Vector<int> > v());
 }
 
 TEST(Vector, can_assign_vectors_of_equal_size)
